e727bd2d3e413a1cd57769ae69d04e5d09867018
denis-aenasoaei <denis.aenasoaei@yahoo.com>
2020-05-10 21:16:14 +0300

    made split lungs testable, added tests for split lungs script

--- UnitTesting/testSplitLungs.py
+++ UnitTesting/testSplitLungs.py
@@ -1,37 +1,38 @@
-from splitLungss import isFinalDir
+from splitLungs import isFinalDir
 import os
 import unittest
+from splitLungs import separateLungs
+import splitLungs
 
 class MyTestCase(unittest.TestCase):
     def test_isFinalDir(self):
-        directory = os.path.abspath('.')
-        self.assertFalse(isFinalDir(directory))
-        self.assertTrue(isFinalDir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\FrontBack"))
+        self.assertFalse(isFinalDir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TestCTRs\\001"))
+        self.assertTrue(isFinalDir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\CT Scan\\FrontBack"))
 
     def test_equalNumberOfFiles(self):
-        onlyleftlung = len(os.listdir( "C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung"))
-        onlyrightlung = len(os.listdir( "C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung"))
+        onlyleftlung = len(os.listdir( "C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung"))
+        onlyrightlung = len(os.listdir( "C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung"))
         self.assertEqual(onlyleftlung,onlyrightlung)
 
     def test_allInputFilesArePng(self):
         first = 0
         second = 0
         third = 0
-        for root, dirs, files in os.walk("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\FrontBack"):
+        for root, dirs, files in os.walk("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\FrontBack"):
             for file in files:
                 if file.endswith(".png"):
                     first = first + 1
-        for root, dirs, files in os.walk("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\LeftRight"):
+        for root, dirs, files in os.walk("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\LeftRight"):
             for file in files:
                 if file.endswith(".png"):
                     second = second + 1
-        for root, dirs, files in os.walk("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\TopBottom"):
+        for root, dirs, files in os.walk("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\TopBottom"):
             for file in files:
                 if file.endswith(".png"):
                     third = third + 1
-        nr1 = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\FrontBack"))
-        nr2 = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\LeftRight"))
-        nr3 = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\TopBottom"))
+        nr1 = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\FrontBack"))
+        nr2 = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\LeftRight"))
+        nr3 = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\TopBottom"))
         self.assertTrue(first,nr1)
         self.assertTrue(second, nr2)
         self.assertTrue(third, nr3)
@@ -41,17 +42,17 @@ class MyTestCase(unittest.TestCase):
         second = 0
         third = 0
         for root, dirs, files in os.walk(
-                "C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung\\FrontBack"):
+                "C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung\\FrontBack"):
             for file in files:
                 if file.endswith(".png"):
                     first = first + 1
         for root, dirs, files in os.walk(
-                "C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung\\LeftRight"):
+                "C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung\\LeftRight"):
             for file in files:
                 if file.endswith(".png"):
                     second = second + 1
         for root, dirs, files in os.walk(
-                "C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung\\TopBottom"):
+                "C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung\\TopBottom"):
             for file in files:
                 if file.endswith(".png"):
                     third = third + 1
@@ -59,26 +60,26 @@ class MyTestCase(unittest.TestCase):
         second1 = 0
         third1 = 0
         for root, dirs, files in os.walk(
-                "C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung\\FrontBack"):
+                "C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung\\FrontBack"):
             for file in files:
                 if file.endswith(".png"):
                     first1 = first1 + 1
         for root, dirs, files in os.walk(
-                "C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung\\LeftRight"):
+                "C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung\\LeftRight"):
             for file in files:
                 if file.endswith(".png"):
                     second1 = second1 + 1
         for root, dirs, files in os.walk(
-                "C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung\\TopBottom"):
+                "C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung\\TopBottom"):
             for file in files:
                 if file.endswith(".png"):
                     third1 = third1 + 1
-        nr1 = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung\\FrontBack"))
-        nr2 = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung\\LeftRight"))
-        nr3 = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung\\TopBottom"))
-        nr11 = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung\\FrontBack"))
-        nr12 = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung\\LeftRight"))
-        nr13 = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung\\TopBottom"))
+        nr1 = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung\\FrontBack"))
+        nr2 = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung\\LeftRight"))
+        nr3 = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung\\TopBottom"))
+        nr11 = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung\\FrontBack"))
+        nr12 = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung\\LeftRight"))
+        nr13 = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung\\TopBottom"))
         self.assertTrue(first, nr1)
         self.assertTrue(second, nr2)
         self.assertTrue(third, nr3)
@@ -87,35 +88,62 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(third1, nr13)
 
     def test_inputDirectoriesExist(self):
-        self.assertTrue(os.path.isdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\TopBottom"))
-        self.assertTrue(os.path.isdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\LeftRight"))
-        self.assertTrue(os.path.isdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\FrontBack"))
+        self.assertTrue(os.path.isdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\TopBottom"))
+        self.assertTrue(os.path.isdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\LeftRight"))
+        self.assertTrue(os.path.isdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\FrontBack"))
 
     def test_outputDirectoriesExist(self):
-        self.assertTrue(os.path.isdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung"))
-        self.assertTrue(os.path.isdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung"))
+        self.assertTrue(os.path.isdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung"))
+        self.assertTrue(os.path.isdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung"))
 
     def test_outputDirectoriesNotEmpty(self):
-        self.assertNotEqual(len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung\\TopBottom")),0)
-        self.assertNotEqual(len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung\\FrontBack")),0)
-        self.assertNotEqual(len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung\\LeftRight")),0)
-        self.assertNotEqual(len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung")),0)
-        self.assertNotEqual(len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung\\TopBottom")),0)
-        self.assertNotEqual(len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung\\FrontBack")),0)
-        self.assertNotEqual(len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung\\LeftRight")),0)
-        self.assertNotEqual(len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung")),0)
+        self.assertNotEqual(len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung\\TopBottom")),0)
+        self.assertNotEqual(len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung\\FrontBack")),0)
+        self.assertNotEqual(len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung\\LeftRight")),0)
+        self.assertNotEqual(len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung")),0)
+        self.assertNotEqual(len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung\\TopBottom")),0)
+        self.assertNotEqual(len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung\\FrontBack")),0)
+        self.assertNotEqual(len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung\\LeftRight")),0)
+        self.assertNotEqual(len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung")),0)
 
     def test_inputDirectoriesNotEmpty(self):
-        self.assertNotEqual(len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\TopBottom")),0)
-        self.assertNotEqual(len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\FrontBack")),0)
-        self.assertNotEqual(len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\LeftRight")),0)
+        self.assertNotEqual(len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\TopBottom")),0)
+        self.assertNotEqual(len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\FrontBack")),0)
+        self.assertNotEqual(len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\LeftRight")),0)
 
-    def test_allLungsHaveBeenSplit(self):
-        frontback = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\FrontBack"))
-        leftlung_frontback = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyLeftLung\\FrontBack"))
-        rightlung_frontback = len(os.listdir("C:\\Users\\User\\Desktop\\Software-Engineering-2020\\1-10\\001\\Masks\\Mask1\\OnlyRightLung\\FrontBack"))
+    '''def test_allLungsHaveBeenSplit(self):
+        frontback = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\FrontBack"))
+        leftlung_frontback = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyLeftLung\\FrontBack"))
+        rightlung_frontback = len(os.listdir("C:\\Users\\denis\\Desktop\\Software-Engineering2020\\TrainCTRs\\001\\Masks\\Mask1\\OnlyRightLung\\FrontBack"))
         self.assertEqual(frontback,leftlung_frontback)
         self.assertEqual(frontback,rightlung_frontback)
+    '''
+    def test_isFinalDirOnEmptyDirShouldBeTrue(self):
+            self.assertEqual(True, isFinalDir("C:\\Users\\denis\\Desktop\\ForTests\\FrontBack"))
+
+    def test_isCTR_dir(self):
+            self.assertEqual(-1, separateLungs("C:\\Users\\denis\\Desktop\\ForTests\\forIsCTRFolder"))
+
+    def test_inputFolderNotEmpty(self):
+        self.assertEqual(-2, separateLungs("C:\\Users\\denis\\Desktop\\ForTests\\FrontBack"))
+
+    def test_notCTRinName(self):
+        self.assertEqual(-3, separateLungs("C:\\Users\\denis\\Desktop\\ForTests\\notCTRFiles\\FrontBack"))
+
+    def test_imagesNotOfSize512ShouldReturnMinusFour(self):
+        self.assertEqual(-4,separateLungs("C:\\Users\\denis\\Desktop\\ForTests\\notSize512\\FrontBack"))
+
+    def test_SplitFrontBack(self):
+        self.assertEqual(1, separateLungs("C:\\Users\\denis\\Desktop\\ForTests\\ShouldWork\\Mask1\\FrontBack"))
+
+    def test_SplitLeftRight(self):
+        self.assertEqual(0, separateLungs("C:\\Users\\denis\\Desktop\\ForTests\\ShouldWork\\Mask1\\LeftRight"))
+
+    def test_SplitTopBottom(self):
+        self.assertEqual(1, separateLungs("C:\\Users\\denis\\Desktop\\ForTests\\ShouldWork\\Mask1\\TopBottom"))
+
+    def test_WorksOkOnFolders(self):
+        self.assertEqual(1, splitLungs.main("C:\\Users\\denis\\Desktop\\ForTests\\TestMain"))
 
 if __name__ == '__main__':
     unittest.main()
