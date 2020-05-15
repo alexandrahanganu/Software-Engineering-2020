cd7a609110e9e72b4290d7242b2830468d05357a
Theodor Samson <61683286+Theo3699@users.noreply.github.com>
2020-05-15 22:44:31 +0300

    Add files via upload
    Fiecare fisier creaza un csv pentru zona cu numele fisierului, iar, la final, trebuie rulat Results pentru a crea csv-ul final

---
+++ Laboratoare/Laborator 12/Samson Theodor/CavernsLeft.py
@@ -0,0 +1,179 @@
+import cv2  # working with, mainly resizing, images
+import numpy as np  # dealing with arrays
+import os  # dealing with directories
+from random import shuffle  # mixing up or currently ordered data that might lead our network astray in training.
+from tqdm import tqdm  # a nice pretty percentage bar for tasks. Thanks to viewer Daniel BA1/4hler for this suggestion
+import csv
+
+from PIL import Image
+
+def def_labels_left_has_caverns(nume_pacient):
+    with open('D:\\Git\\CT_Report.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == nume_pacient:
+                if row[4] == '1':
+                    return [1, 0]  # has caverns
+                if row[4] == '0':
+                    return [0, 1]  # doesn't have caverns
+                break
+
+
+# getting the data
+TRAIN_DIR = 'D:\\Git\\Processed-Images-Left'
+TEST_DIR = 'D:\\Git\\test'
+IMG_SIZE = 64
+
+def create_train_data_for_tbc():
+    training_data = []
+    count = 0
+    subfolders = [f.path for f in os.scandir(TRAIN_DIR) if f.is_dir()]
+    for sub in subfolders:
+        print(sub[-3:])
+        label = def_labels_left_has_caverns(sub[-3:])
+        path = os.path.join(TRAIN_DIR, sub[-3:] + '\\FrontBack')
+        for root, dirs, files in os.walk(path):
+            # print("luam alt pacient")
+            for name in files:
+                # print("sunt aici la poza fiecarui pacient")
+                count = count + 1
+                img = cv2.imread(root + '\\' + name)
+                #img = cv2.imread(root + '\\' + name)
+                img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+                training_data.append([np.array(img), np.array(label)])
+
+            path = os.path.join(TRAIN_DIR, sub[-3:] + '\\LeftRight')
+            for root, dirs, files in os.walk(path):
+                # print("luam alt pacient")
+                for name in files:
+                    # print("sunt aici la poza fiecarui pacient")
+                    count = count + 1
+                    img = cv2.imread(root + '\\' + name)
+                    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+
+                    training_data.append([np.array(img), np.array(label)])
+
+            path = os.path.join(TRAIN_DIR, sub[-3:] + '\\TopBottom')
+            for root, dirs, files in os.walk(path):
+                # print("luam alt pacient")
+                for name in files:
+                    # print("sunt aici la poza fiecarui pacient")
+                    count = count + 1
+                    img = cv2.imread(root + '\\' + name)
+                    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+
+                    training_data.append([np.array(img), np.array(label)])
+
+        if int(sub[-3:]) == 100:
+            break
+
+    shuffle(training_data)
+    # np.save('train_data.npy', training_data)
+    print(count)
+    return training_data
+
+
+def create_test_data():
+    test_data = []
+    count = 0
+    subfolders = [f.path for f in os.scandir(TEST_DIR) if f.is_dir()]
+    for sub in subfolders:
+        print(sub[-3:])
+
+        testPath = os.path.join(TEST_DIR, sub[-3:] + '\\FrontBack')
+        for root, dirs, files in os.walk(testPath):
+            for name in files:
+                count = count+1
+                img = cv2.imread(root + '\\' + name)
+                #img = cv2.imread(root + '\\' + name)
+                img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+
+                test_data.append([np.array(img), name])
+
+            testPath = os.path.join(TEST_DIR, sub[-3:] + '\\LeftRight')
+            for root, dirs, files in os.walk(testPath):
+                for name in files:
+                    img = cv2.imread(root + '\\' + name)
+                    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+
+                    test_data.append([np.array(img), name])
+
+            testPath = os.path.join(TEST_DIR, sub[-3:] + '\\TopBottom')
+            for root, dirs, files in os.walk(testPath):
+                for name in files:
+                    img = cv2.imread(root + '\\' + name)
+                    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+
+                    test_data.append([np.array(img), name])
+
+        if int(sub[-3:]) == 188:
+            break
+
+    # shuffle(test_data)
+    # np.save('test_data.npy', test_data)
+    return test_data
+
+
+#reshape data to fit model
+
+train_data = create_train_data_for_tbc()
+test_data = create_test_data()
+
+train = train_data[:-500]
+test = train_data[-500:]
+
+easy = 3
+
+X = np.array([i[0] for i in train]).reshape(-1, IMG_SIZE, IMG_SIZE, easy)
+Y = np.array([i[1] for i in train])
+
+test_x = np.array([i[0] for i in test]).reshape(-1, IMG_SIZE, IMG_SIZE, easy)
+test_y = np.array([i[1] for i in test])
+
+
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+#create model
+model = Sequential()
+#add model layers
+model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, easy)))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(2, activation='softmax'))
+
+#compile model using accuracy to measure model performance
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+model.fit(X, Y, batch_size = 200, validation_data=(test_x, test_y), epochs=3)
+
+
+with open('Caverns_Results_Left.csv', 'w') as f:
+    f.write('id,label\n')
+
+with open('Caverns_Results_Left.csv', 'a') as f:
+    value = '178'
+    avg = 0
+    cont = 0
+    valoare = 0
+    for data in tqdm(test_data):
+        img_num = data[1]
+
+        if value in img_num:
+
+            cont = cont + 1
+            img_data = data[0]
+            data = img_data.reshape(-1, IMG_SIZE, IMG_SIZE, easy)
+            model_out = model.predict([data])[0]
+            avg = avg + model_out[0]
+        else:
+            valoare = avg / cont
+            f.write('{}, {}\n'.format(value, valoare))
+            value1 = int(value)
+            value1 = value1 + 1
+            value = str(value1)
+
+            avg = 0
+            cont = 0
+    valoare = avg / cont
+    f.write('{}, {}\n'.format(value, valoare))
---
+++ Laboratoare/Laborator 12/Samson Theodor/Results.py
@@ -0,0 +1,133 @@
+# def def_labels_right_has_Tuberculosis(nume_pacient):
+#     with open('D:\\Git\\CT_Report.csv', 'r') as file:
+#         reader = csv.reader(file)
+#         for row in reader:
+#             if row[0] == nume_pacient:
+#                 if row[2] == '1':
+#                     return [1, 0]  # has tbc
+#                 if row[2] == '0':
+#                     return [0, 1]  # doesn't have tbc
+#                 break
+
+
+
+# with open('C:\\Users\\Theo\\PycharmProjects\\TuberculosisProject\\TBC_Results_Left.csv', 'r') as file:
+#     with open('Final_Results.csv', 'a') as f:
+#         rowIndex = []
+#         tbcList = []
+#         reader = csv.reader(file)
+#         i = 0
+#         j = 0
+#         for row in reader:
+#             id = row[0]
+#             TBCpercent = row[1]
+#             rowIndex.append(id)
+#             tbcList.append(TBCpercent)
+#             #fa liste pt fiecare coloana si apoi scrie
+#
+#         print (rowIndex)
+#         print (tbcList)
+#         print (rowIndex[0])
+#         print (tbcList[0])
+#         print (row_count)
+
+
+
+
+
+import csv
+
+with open('Final_Results.csv', 'w') as f:
+    f.write('Patient, LeftTuberculosis, RightTuberculosis, CavernsLeft, CavernsRight, PleurisyLeft, PleurisyRight\n')
+
+
+with open('C:\\Users\\Theo\\PycharmProjects\\TuberculosisProject\\TBC_Results_Left.csv', 'r') as file:
+    row_count = sum(1 for row in file)#in order to know how many lines there are in each csv
+
+
+with open('C:\\Users\\Theo\\PycharmProjects\\TuberculosisProject\\TBC_Results_Left.csv', 'r') as tbcleft:
+    rowIndex = []
+    tbcleftpercentage = []
+    reader = csv.reader(tbcleft)
+    for row in reader:
+        rowIndex.append(row[0])
+        tbcleftpercentage.append(row[1])
+
+
+with open('C:\\Users\\Theo\\PycharmProjects\\TuberculosisProject\\TBC_Results_Right.csv', 'r') as tbcright:
+    tbcrightpercentage = []
+    reader = csv.reader(tbcright)
+    for row in reader:
+        tbcrightpercentage.append(row[1])
+
+
+with open('C:\\Users\\Theo\\PycharmProjects\\TuberculosisProject\\Caverns_Results_Left.csv', 'r') as cavernsleft:
+    clpercentage = []
+    reader = csv.reader(cavernsleft)
+    for row in reader:
+        clpercentage.append(row[1])
+    print (clpercentage)
+
+
+with open('C:\\Users\\Theo\\PycharmProjects\\TuberculosisProject\\Caverns_Results_Right.csv', 'r') as cavernsright:
+    crpercentage = []
+    reader = csv.reader(cavernsright)
+    for row in reader:
+        crpercentage.append(row[1])
+    print (crpercentage)
+
+
+with open('C:\\Users\\Theo\\PycharmProjects\\TuberculosisProject\\Pleurisy_Results_Left.csv', 'r') as pleurisyleft:
+    plpercentage = []
+    reader = csv.reader(pleurisyleft)
+    for row in reader:
+        plpercentage.append(row[1])
+    print (plpercentage)
+
+
+with open('C:\\Users\\Theo\\PycharmProjects\\TuberculosisProject\\Pleurisy_Results_Right.csv', 'r') as pleurisyright:
+    prpercentage = []
+    reader = csv.reader(pleurisyright)
+    for row in reader:
+        prpercentage.append(row[1])
+    print (prpercentage)
+
+
+with open('Final_Results.csv', 'a') as f:
+    i = 1
+    for index in range(row_count-1):
+        f.write('{}, {}, {}, {}, {}, {}, {}\n'.format(rowIndex[i], tbcleftpercentage[i], tbcrightpercentage[i],
+                                                  clpercentage[i], crpercentage[i],
+                                                  plpercentage[i], prpercentage[i]))
+        i = i + 1
+
+
+# with open('TBC_Results_Right.csv', 'w') as f:
+#     f.write('id,label\n')
+#
+# with open('TBC_Results_Right.csv', 'a') as f:
+#     value = '178'
+#     avg = 0
+#     cont = 0
+#     valoare = 0
+#     for data in tqdm(test_data):
+#         img_num = data[1]
+#
+#         if value in img_num:
+#
+#             cont = cont + 1
+#             img_data = data[0]
+#             data = img_data.reshape(-1, IMG_SIZE, IMG_SIZE, easy)
+#             model_out = model.predict([data])[0]
+#             avg = avg + model_out[0]
+#         else:
+#             valoare = avg / cont
+#             f.write('{}, {}\n'.format(value, valoare))
+#             value1 = int(value)
+#             value1 = value1 + 1
+#             value = str(value1)
+#
+#             avg = 0
+#             cont = 0
+#     valoare = avg / cont
+#     f.write('{}, {}\n'.format(value, valoare))
